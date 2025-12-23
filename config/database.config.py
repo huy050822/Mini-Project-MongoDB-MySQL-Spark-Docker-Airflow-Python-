aec88cd3844f4d9d5a7c6328f21f1af07e71dd3a
huy050822 <huy.tnq@gmail.com>
2025-12-23 22:25:05 +0700

    Config mysql

---
+++ config/database.config.py
@@ -0,0 +1,44 @@
+# Get thông tin database từ file .env 
+
+import os
+from dotenv import load_dotenv
+from dataclasses import dataclass
+
+#Config MySql
+
+# host = os.getenv("MYSQL_HOST")
+# port = os.getenv("MYSQL_PORT")
+# user = os.getenv("MYSQL_USER")
+# password = os.getenv("MYSQL_PASSWORD")
+# database = os.getenv("MYSQL_DATABASE")
+# print(host,port,user,database,password)
+
+
+@dataclass
+class Mysql_Config():
+    host : str
+    port : int
+    user : str
+    password : str
+    database : str
+    
+
+def get_database_config():
+    load_dotenv()
+
+    config = {
+        "mysql" : Mysql_Config(
+            host = os.getenv("MYSQL_HOST"),
+            port = os.getenv("MYSQL_PORT"),
+            user = os.getenv("MYSQL_USER"),
+            password = os.getenv("MYSQL_PASSWORD"),
+            database = os.getenv("MYSQL_DATABASE")
+        ),
+        "mongodb" : ""
+    }
+
+    return config
+
+
+config = get_database_config()
+print(config)
